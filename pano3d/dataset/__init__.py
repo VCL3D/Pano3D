d8f455137d6dcdda10d61386bf696d2e595fe223
Nikolaos Zioulis <nzioulis@iti.gr>
2021-11-08 16:52:31 +0200

    add loading

--- pano3d/dataset/__init__.py
+++ pano3d/dataset/__init__.py
@@ -1,34 +1,146 @@
-from pano3d.dataset.gibson_v2 import GibsonV2
-from pano3d.dataset.matterport3d import Matterport3D
-
+import os
+import sys
 import torch
 import toolz
+import yaml
+import pathlib
 import typing
 import logging
+import functools
+
+from loaders import (
+    load_color,
+    load_depth,
+    load_normal,
+    load_semantic,
+    load_structure,
+    load_layout,
+)
 
 log = logging.getLogger(__name__)
 
 __all__ = ['Pano3D']
 
-__DATASETS__ = {
-    'm3d': Matterport3D,
-    'gv2': GibsonV2,
-}
+class Pano3D(torch.utils.data.Dataset):
 
-def _split(dataset: str) -> typing.Tuple[str, str]:
-    s = dataset.split('_')
-    return toolz.first(s), toolz.second(s)
+    __LOADERS__ = {
+        'color': load_color,
+        'raw': load_color,
+        'emission': load_color,
+        'depth': load_depth,
+        'normal': load_normal,
+        'semantic': load_semantic,
+        'structure': load_structure,
+        'layout': load_layout,
+        'color_up': functools.partial(load_color, position='up'),
+        'color_down': functools.partial(load_color, position='down'),
+        'color_left': functools.partial(load_color, position='left'),
+        'color_right': functools.partial(load_color, position='right'),
+        'depth_up': functools.partial(load_depth, position='up'),
+        'depth_down': functools.partial(load_depth, position='down'),
+        'depth_left': functools.partial(load_depth, position='left'),
+        'depth_right': functools.partial(load_depth, position='right'),
+        'normal_up': functools.partial(load_normal, position='up'),
+        'normal_down': functools.partial(load_normal, position='down'),
+        'normal_left': functools.partial(load_normal, position='left'),
+        'normal_right': functools.partial(load_normal, position='right'),
+        'semantic_up': functools.partial(load_semantic, position='up'),
+        'semantic_down': functools.partial(load_semantic, position='down'),
+        'semantic_left': functools.partial(load_semantic, position='left'),
+        'semantic_right': functools.partial(load_semantic, position='right'),
+        'structure_up': functools.partial(load_structure, position='up'),
+        'structure_down': functools.partial(load_structure, position='down'),
+        'structure_left': functools.partial(load_structure, position='left'),
+        'structure_right': functools.partial(load_structure, position='right'),
+        'layout_up': functools.partial(load_layout, position='up'),
+        'layout_down': functools.partial(load_layout, position='down'),
+        'layout_left': functools.partial(load_layout, position='left'),
+        'layout_right': functools.partial(load_layout, position='right'),
+    }
 
-class Pano3D(torch.utils.data.ConcatDataset):
     def __init__(self,
-        root:       str,
-        datasets:   typing.Sequence[str],
-        splits:     typing.Sequence[str],
-        tensors:    typing.Sequence[str],
+        root:       typing.Union[str, pathlib.Path],
+        part:       str,
+        split:      typing.Union[str, pathlib.Path],
+        types:      typing.Sequence[str],
+        max_depth:  float=8.0,
     ):
-        super(Pano3D, self).__init__(
-            __DATASETS__[d](root, p, s, tensors) for (d, p), s in zip(
-                map(_split, datasets), 
-                splits
-            )
+        folder = self._get_folder(root, part)
+        self.filenames = self._get_filenames(folder, split)
+        self.loaders = [
+            functools.partial(Pano3D.__LOADERS__[type], 
+                max_depth=max_depth
+            ) for type in types
+        ]
+
+    def _get_folder(self, root: typing.Union[str, pathlib.Path], part: str) -> str:
+        if not os.path.exists(root):
+            log.error((
+                f"Invalid Pano3D root folder ({root}), "
+                "please use the folder where the Zenodo archives have been extracted to."
+            ))
+            sys.exit(2)
+        part_path = os.path.join(root, part)
+        if not os.path.exists(part_path):
+            log.error((
+                "Invalid Pano3D part folder, "
+                f"please use the correct part folder's name inside the root directory ({root})."
+            ))
+            sys.exit(2)
+        return part_path
+
+    def _get_filenames(self,
+        folder: str, split: typing.Union[str, pathlib.Path]
+    ) -> typing.Sequence[typing.Union[str, pathlib.Path]]:
+        if not os.path.exists(split):
+            log.error((
+                f"Pano3D split file ({split}) does not exist. "
+            ))
+            sys.exit(2)
+        with open(split) as f:
+            data_filenames = yaml.load(f, Loader=yaml.FullLoader)
+        filenames = []
+        for building, renders in data_filenames.items():
+            for render in renders:
+                filenames.append(os.path.join(folder, building, render))
+        return filenames
+
+    def __len__(self) -> int:
+        return len(self.filenames)
+
+    def __getitem__(self, index: int) -> typing.Mapping[str, torch.Tensor]:
+        out = { }
+        filename = self.filenames[index]
+        for loader in self.loaders:
+            out = toolz.merge(out, loader(filename))
+        return out
+
+if __name__ == "__main__":
+    import argparse
+    def parse_arguments(args):
+        usage_text = (
+            "Test Pano3D data loading."
         )
+        parser = argparse.ArgumentParser(description=usage_text)
+        parser.add_argument("--pano3d_root", type=str, help="Path to the root folder containing the Pano3D extracted data.")
+        parser.add_argument("--pano3d_part", type=str, help="The Pano3D subset to load.")
+        parser.add_argument("--pano3d_split", type=str, help="The Pano3D split corresponding to the selected subset that will be loaded.")
+        parser.add_argument('--pano3d_types', default='color', nargs='+',
+            choices=[
+                'color', 'depth', 'normal', 'semantic', 'structure', 'layout',
+                'color_up', 'depth_up', 'normal_up', 'semantic_up', 'structure_up', 'layout_up'
+                'color_down', 'depth_down', 'normal_down', 'semantic_down', 'structure_down', 'layout_down'
+                'color_left', 'depth_left', 'normal_left', 'semantic_left', 'structure_left', 'layout_left'
+                'color_right', 'depth_right', 'normal_right', 'semantic_right', 'structure_right', 'layout_right'
+            ],
+            help='The Pano3D data types that will be loaded, one of [color, depth, normal, semantic, structure, layout], potentially suffixed with a stereo placement from [up, down, left, right].'
+        )
+        return parser.parse_known_args(args)
+    args, unknown = parse_arguments(sys.argv)
+    dataset = Pano3D(
+        root=args.pano3d_root,
+        part=args.pano3d_part,
+        split=args.pano3d_split,
+        types=args.pano3d_types,
+    )
+    print(f"Loaded {len(dataset)} items.")
---
+++ pano3d/dataset/loaders.py
@@ -0,0 +1,66 @@
+import typing
+import torch
+import torchvision
+import cv2
+import numpy
+
+def load_color(filename: str, **kwargs) -> torch.Tensor:    
+    if 'position' in kwargs:
+        filename = filename.replace('center', kwargs['position'])
+    return {'color': torchvision.io.read_image(filename) / 255.0 }    
+
+def load_depth(filename: str, max_depth: float=8.0, **kwargs) -> torch.Tensor:
+    if 'position' in kwargs:
+        filename = filename.replace('center', kwargs['position'])
+    depth = torch.from_numpy(cv2.imread(
+        filename.replace('emission', 'depth').replace('.png', '.exr'), 
+        cv2.IMREAD_ANYDEPTH
+    )).unsqueeze(0)
+    depth[depth > max_depth] = 0.0
+    return {
+        'depth': depth
+    }
+
+def load_normal(filename: str, **kwargs) -> torch.Tensor:
+    if 'position' in kwargs:
+        filename = filename.replace('center', kwargs['position'])
+    return {
+        'normal': torch.from_numpy(cv2.imread(
+            filename.replace('emission', 'normal_map').replace('.png', '.exr'), 
+            cv2.IMREAD_ANYDEPTH | cv2.IMREAD_ANYCOLOR
+        ).transpose(2, 0, 1))
+    }
+
+def load_semantic(filename: str, **kwargs) -> torch.Tensor:
+    if 'position' in kwargs:
+        filename = filename.replace('center', kwargs['position'])
+    return {
+        'semantic': torch.from_numpy(cv2.imread(
+            filename.replace('emission', 'semantic').replace('.png', '.exr')
+        )).long()
+    }
+
+def load_structure(filename: str, **kwargs) -> torch.Tensor:
+    if 'position' in kwargs:
+        filename = filename.replace('center', kwargs['position'])
+    return { 
+        'structure': torch.from_numpy(cv2.imread(
+            filename.replace('emission', 'layout').replace('.png', '.exr')
+        )).long()
+    }
+
+def load_layout(filename: str, **kwargs) -> typing.Mapping[str, torch.Tensor]:
+    if 'position' in kwargs:
+        filename = filename.replace('center', kwargs['position'])
+    ret = { }
+    with numpy.load(filename) as f:
+        layout = f['arr_0.npy']
+    ret["top_layout"] = torch.from_numpy(layout[0, ...])
+    ret["top_weights"] = (ret["top_layout"] > 0).float()
+    ret["top_layout"] = (128. - ret["top_layout"]) / 128.
+    ret["bottom_layout"] = torch.from_numpy(layout[1, ...])
+    ret["bottom_weights"] = (ret["bottom_layout"] > 0).float()
+    ret["bottom_layout"] = (ret["bottom_layout"] - 128.) / 128.
+    ret["top_layout"] *= ret["top_weights"]
+    ret["bottom_layout"] *= ret["bottom_weights"]
+    return ret
